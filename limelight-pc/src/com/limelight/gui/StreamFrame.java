18ee201d5a11ad06ab80e4bfb125fb640870cde8
Diego Waxemberg <diego.waxemberg@gmail.com>
2013-12-05 21:31:02 -0500

    stream frame is now built. frames are now created centered. now show appname on osx instead of com.limelight.limelight

--- limelight-pc/src/com/limelight/gui/StreamFrame.java
+++ limelight-pc/src/com/limelight/gui/StreamFrame.java
@@ -1,11 +1,18 @@
 package com.limelight.gui;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class StreamFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
 	public void build() {
-		
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setSize(1280,720);
+		//This might break if the screen res is too small...not sure though
+		this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+		this.setVisible(true);
 	}
 }

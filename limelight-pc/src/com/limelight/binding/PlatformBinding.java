0f9d597386375433660d3c59c283bf559dadf463
Cameron Gutman <aicommander@gmail.com>
2013-12-05 19:10:24 -0500

    Fix UI jank. Add support for pairing. Add missing XML parsing jar.

--- limelight-pc/src/com/limelight/binding/PlatformBinding.java
+++ limelight-pc/src/com/limelight/binding/PlatformBinding.java
@@ -1,5 +1,8 @@
 package com.limelight.binding;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import com.limelight.binding.audio.JavaxAudioRenderer;
 import com.limelight.binding.video.SwingCpuDecoderRenderer;
 import com.limelight.nvstream.av.audio.AudioRenderer;
@@ -11,7 +14,11 @@ public class PlatformBinding {
 	}
 	
 	public static String getDeviceName() {
-        return "foobar";
+		try {
+			return InetAddress.getLocalHost().getHostName();
+		} catch (UnknownHostException e) {
+			return "LimelightPC";
+		}
 	}
 	
 	public static AudioRenderer getAudioRenderer() {

f1984f08b7531d974481e4694bd267ab09ae8b19
Diego Waxemberg <diego.waxemberg@gmail.com>
2013-12-07 21:22:52 -0500

    added support for keyboard keys a-z

--- limelight-pc/src/com/limelight/gui/StreamFrame.java
+++ limelight-pc/src/com/limelight/gui/StreamFrame.java
@@ -5,10 +5,17 @@ import java.awt.Toolkit;
 
 import javax.swing.JFrame;
 
+import com.limelight.input.KeyboardHandler;
+import com.limelight.nvstream.NvConnection;
+
 public class StreamFrame extends JFrame {
 	private static final long serialVersionUID = 1L;
 	
-	public void build() {
+	private KeyboardHandler keyboard;
+
+	public void build(NvConnection conn) {
+		keyboard = new KeyboardHandler(conn);
+		this.addKeyListener(keyboard);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		this.setSize(1280,720);
 		//This might break if the screen res is too small...not sure though

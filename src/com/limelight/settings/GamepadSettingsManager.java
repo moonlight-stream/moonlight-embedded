248864a604b75a7d9578ceb770094db7d07433c5
Diego Waxemberg <diego.waxemberg@gmail.com>
2013-12-29 13:16:02 -0500

    added javadoc to settings classes

--- src/com/limelight/settings/GamepadSettingsManager.java
+++ src/com/limelight/settings/GamepadSettingsManager.java
@@ -4,10 +4,17 @@ import java.io.File;
 
 import com.limelight.input.gamepad.GamepadMapping;
 
+/**
+ * Manages the gamepad settings
+ * @author Diego Waxemberg
+ */
 public abstract class GamepadSettingsManager {
 	private static GamepadMapping cachedSettings;
 
-
+	/**
+	 * Reads the gamepad settings from the gamepad file and caches the configuration
+	 * @return the gamepad settings
+	 */
 	public static GamepadMapping getSettings() {
 		if (cachedSettings == null) {
 			System.out.println("Reading Gamepad Settings");
@@ -23,6 +30,10 @@ public abstract class GamepadSettingsManager {
 		return cachedSettings;
 	}
 
+	/**
+	 * Writes the specified mapping to the gamepad file and updates the cached settings
+	 * @param settings the new gamepad mapping to be written out
+	 */
 	public static void writeSettings(GamepadMapping settings) {
 		cachedSettings = settings;
 		System.out.println("Writing Gamepad Settings");

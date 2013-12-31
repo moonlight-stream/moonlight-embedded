f87a085a36d5cf7c5fe0162a5e2ec43d23ebdf85
Cameron Gutman <aicommander@gmail.com>
2013-12-30 23:53:56 -0600

    Add gamepad_jni code and libraries for Win32 and Win64

---
+++ jni/gamepad_jni/buildwin.sh
@@ -0,0 +1,4 @@
+rm gamepad_jni.dll gamepad_jni64.dll
+/C/MinGW/bin/gcc -shared -I"/C/Program Files (x86)/Java/jdk1.7.0_45/include" -I"/C/Program Files (x86)/Java/jdk1.7.0_45/include/win32" *.c -L./win32 -lstem_gamepad -lxinput1_3 -Wl,--no-undefined -Wl,--kill-at -o gamepad_jni.dll
+/C/MinGW-w64/bin/gcc -shared -I"/C/Program Files (x86)/Java/jdk1.7.0_45/include" -I"/C/Program Files (x86)/Java/jdk1.7.0_45/include/win32" *.c -L./win64 -lstem_gamepad -lxinput1_3 -Wl,--no-undefined -Wl,--kill-at -o gamepad_jni64.dll
+

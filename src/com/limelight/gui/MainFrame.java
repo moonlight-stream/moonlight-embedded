682aa6d205fdf9fe6754d70440a1f64195b40003
Cameron Gutman <aicommander@gmail.com>
2014-01-01 15:34:07 -0600

    Fix unused import warnings

--- src/com/limelight/gui/MainFrame.java
+++ src/com/limelight/gui/MainFrame.java
@@ -6,8 +6,6 @@ import java.awt.Dimension;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.SocketException;
@@ -28,7 +26,6 @@ import org.xmlpull.v1.XmlPullParserException;
 
 import com.limelight.Limelight;
 import com.limelight.binding.PlatformBinding;
-import com.limelight.input.gamepad.NativeGamepad;
 import com.limelight.nvstream.NvConnection;
 import com.limelight.nvstream.http.NvHTTP;
 import com.limelight.settings.PreferencesManager;

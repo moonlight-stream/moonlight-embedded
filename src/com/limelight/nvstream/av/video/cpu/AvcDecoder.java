51d112306f94406e0ac1dc37f87f0f525be6e4cd
Cameron Gutman <aicommander@gmail.com>
2014-01-01 19:29:51 -0500

    Fix loading native libraries on OS X

--- src/com/limelight/nvstream/av/video/cpu/AvcDecoder.java
+++ src/com/limelight/nvstream/av/video/cpu/AvcDecoder.java
@@ -4,16 +4,17 @@ import com.limelight.binding.LibraryHelper;
 
 public class AvcDecoder {
 	static {
-		LibraryHelper.loadNativeLibrary("avutil-52");
+		// Windows uses runtime linking for ffmpeg libraries
 		if (System.getProperty("os.name").contains("Windows")) {
+			LibraryHelper.loadNativeLibrary("avutil-52");
 			LibraryHelper.loadNativeLibrary("postproc-52");
 			LibraryHelper.loadNativeLibrary("pthreadVC2");
+			LibraryHelper.loadNativeLibrary("swresample-0");
+			LibraryHelper.loadNativeLibrary("swscale-2");
+			LibraryHelper.loadNativeLibrary("avcodec-55");
+			LibraryHelper.loadNativeLibrary("avformat-55");
+			LibraryHelper.loadNativeLibrary("avfilter-3");
 		}
-		LibraryHelper.loadNativeLibrary("swresample-0");
-		LibraryHelper.loadNativeLibrary("swscale-2");
-		LibraryHelper.loadNativeLibrary("avcodec-55");
-		LibraryHelper.loadNativeLibrary("avformat-55");
-		LibraryHelper.loadNativeLibrary("avfilter-3");
 		
 		LibraryHelper.loadNativeLibrary("nv_avc_dec");
 	}

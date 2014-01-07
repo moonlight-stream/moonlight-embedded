d69a384a5c4bfa237de42cef3e606074845987f6
Iwan Timmer <irtimmer@gmail.com>
2014-01-07 01:43:17 +0100

    Remove software decoder

--- jni/nv_avc_dec/build.sh
+++
@@ -1,5 +0,0 @@
-rm *.o libnv_avc_dec.so
-gcc -I /usr/lib/jvm/java-7-openjdk-amd64/include/ -I ./inc -fPIC -L. -c *.c
-gcc -shared -Wl,-soname,libnv_avc_dec.so -Wl,--no-undefined -o libnv_avc_dec.so *.o -L. -lavcodec -lavfilter -lavformat -lavutil -lswresample -lswscale
-rm *.o
-

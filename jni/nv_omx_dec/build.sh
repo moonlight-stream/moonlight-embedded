b0dd6a9ad51e3a248aeed832963a6d158990617c
Iwan Timmer <irtimmer@gmail.com>
2014-01-08 21:49:25 +0100

    Update build scripts to make building easier

--- jni/nv_omx_dec/build.sh
+++ jni/nv_omx_dec/build.sh
@@ -1,5 +1,5 @@
 rm *.o libnv_omx_dec.so
-gcc -I /opt/vc/src/hello_pi/libs/ilclient -I /usr/lib/jvm/java-7-openjdk/include -I /usr/lib/jvm/java-7-openjdk/include/linux -I /opt/vc/include -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -I ./inc -fPIC -L. -c *.c
+gcc -I /opt/vc/src/hello_pi/libs/ilclient -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -I /opt/vc/include -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -I ./inc -fPIC -L. -c *.c
 gcc -shared -Wl,-soname,libnv_omx_dec.so -Wl,--no-undefined -o libnv_omx_dec.so *.o -L. -L /opt/vc/lib/ -lbcm_host -lopenmaxil -lvcos -lvchiq_arm -lpthread -lrt -L /opt/vc/src/hello_pi/libs/ilclient -lilclient
 rm *.o
 

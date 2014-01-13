2faff379c83df8736177ee2a04e65cc3f9655733
Iwan Timmer <irtimmer@gmail.com>
2014-01-13 17:52:29 +0100

    Use headers on system for building Opus

--- jni/nv_opus_dec/build.sh
+++ jni/nv_opus_dec/build.sh
@@ -1,4 +1,4 @@
 rm *.o libnv_opus_dec.so
-gcc -I include -I $JAVA_HOME/include/ -I $JAVA_HOME/include/linux -I ./inc -fPIC -c *.c
+gcc -I include -I $JAVA_HOME/include/ -I $JAVA_HOME/include/linux -I /usr/include/opus -fPIC -c *.c
 gcc -shared -Wl,-soname,libnv_opus_dec.so -Wl,--no-undefined -o libnv_opus_dec.so *.o -L. -lopus
 rm *.o

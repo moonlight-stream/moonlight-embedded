b9a6f5162f052453964781beef0120b0581ae10f
Iwan Timmer <irtimmer@gmail.com>
2014-05-07 21:56:57 +0200

    Replace build scripts with Makefiles for faster recompiles

--- jni/nv_alsa/build.sh
+++
@@ -1,4 +0,0 @@
-rm *.o libnv_alsa.so
-gcc -I $JAVA_HOME/include -I $JAVA_HOME/include/linux -I /opt/vc/include -I/opt/vc/include/interface/vcos/pthreads -I/opt/vc/include/interface/vmcs_host/linux -I ./inc -fPIC -L. -c *.c
-gcc -shared -Wl,-soname,libnv_alsa.so -Wl,--no-undefined -o libnv_alsa.so *.o -L. -lasound
-rm *.o

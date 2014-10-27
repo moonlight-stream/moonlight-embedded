d53782e4d8867d26ea915afc6793a13a8c304dab
Iwan Timmer <irtimmer@gmail.com>
2014-10-27 17:48:17 +0100

    Video bandwith and latency only during debugging

--- src/com/limelight/Limelight.java
+++ src/com/limelight/Limelight.java
@@ -56,7 +56,7 @@ public class Limelight implements NvConnectionListener {
 	/*
 	 * Creates a connection to the host and starts up the stream.
 	 */
-	private void startUp(StreamConfiguration streamConfig, List<String> inputs, String mappingFile, String audioDevice, boolean tests) {
+	private void startUp(StreamConfiguration streamConfig, List<String> inputs, String mappingFile, String audioDevice, boolean tests, boolean debug) {
 		if (tests) {
 			boolean test = true;
 			String vm = System.getProperties().getProperty("java.vm.name");
@@ -105,7 +105,7 @@ public class Limelight implements NvConnectionListener {
 		conn.start(PlatformBinding.getDeviceName(), null,
 				VideoDecoderRenderer.FLAG_PREFER_QUALITY,
 				PlatformBinding.getAudioRenderer(audioDevice),
-				PlatformBinding.getVideoDecoderRenderer());
+				PlatformBinding.getVideoDecoderRenderer(debug));
 	}
 	
 	/*
@@ -389,7 +389,7 @@ public class Limelight implements NvConnectionListener {
 			if (action.equals("fake"))
 				limelight.startUpFake(streamConfig, video);
 			else
-				limelight.startUp(streamConfig, inputs, mapping, audio, tests);
+				limelight.startUp(streamConfig, inputs, mapping, audio, tests, debug.intValue() > Level.WARNING.intValue());
 		} else if (action.equals("pair"))
 			limelight.pair();
 		else if (action.equals("list"))

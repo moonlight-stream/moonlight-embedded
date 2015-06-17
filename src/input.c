cb56c05c613d9314b5708fa671b83a2867fc3e3e
Gustavo <gustavobenn@hotmail.com>
2015-06-17 11:05:36 -0300

    Simple CEC input

--- src/input.c
+++ src/input.c
@@ -40,6 +40,10 @@
 #include <unistd.h>
 #include <pthread.h>
 
+#ifdef HAVE_LIBCEC
+#include <ceccloader.h>
+#endif
+
 struct input_abs_parms {
   int min, max;
   int flat;
@@ -80,6 +84,93 @@ static struct udev_monitor *udev_mon;
 static int udev_fdindex;
 
 static int sig_fdindex;
+
+#ifdef HAVE_LIBCEC
+static libcec_configuration g_config;
+static char                 g_strPort[50] = { 0 };
+static libcec_interface_t   g_iface;
+static ICECCallbacks        g_callbacks;
+
+static int on_cec_keypress(void *UNUSED, const cec_keypress key)
+{
+  char value;
+  switch (key.keycode)
+  {
+    case CEC_USER_CONTROL_CODE_UP:
+      value = KEY_UP;
+      break;
+    case CEC_USER_CONTROL_CODE_DOWN:
+      value = KEY_DOWN;
+      break;
+    case CEC_USER_CONTROL_CODE_LEFT:
+      value = KEY_LEFT;
+      break;
+    case CEC_USER_CONTROL_CODE_RIGHT:
+      value = KEY_RIGHT;
+      break;
+    case CEC_USER_CONTROL_CODE_ENTER:
+    case CEC_USER_CONTROL_CODE_SELECT:
+      value = KEY_ENTER;
+      break;
+    case CEC_USER_CONTROL_CODE_ROOT_MENU:
+      value = KEY_TAB;
+      break;
+    case CEC_USER_CONTROL_CODE_AN_RETURN:
+    case CEC_USER_CONTROL_CODE_EXIT:
+      value = KEY_ESC;
+      break;
+    default:
+      value = 0;
+      break;
+  }
+  
+  if (value != 0)
+  {
+    short code = 0x80 << 8 | keyCodes[value];
+    LiSendKeyboardEvent(code, (key.duration > 0)?KEY_ACTION_DOWN:KEY_ACTION_UP, 0);
+  }
+}
+
+static void init_cec()
+{
+  libcecc_reset_configuration(&g_config);
+  g_config.clientVersion = LIBCEC_VERSION_CURRENT;
+  g_config.bActivateSource = 0;
+  g_callbacks.CBCecKeyPress = &on_cec_keypress;
+  g_config.callbacks = &g_callbacks;
+  snprintf(g_config.strDeviceName, sizeof(g_config.strDeviceName), "Moonlight");
+  g_config.callbacks = &g_callbacks;
+  g_config.deviceTypes.types[0] = CEC_DEVICE_TYPE_PLAYBACK_DEVICE;
+  
+  if (libcecc_initialise(&g_config, &g_iface, NULL) != 1) {
+    fprintf(stderr, "Failed to initialize libcec interface\n");
+    fflush(stderr);
+    return;
+  }
+  
+  g_iface.init_video_standalone(g_iface.connection);
+  
+  cec_adapter devices[10];
+  int8_t iDevicesFound = g_iface.find_adapters(g_iface.connection, devices, sizeof(devices) / sizeof(devices), NULL);
+  
+  if (iDevicesFound <= 0) {
+    fprintf(stderr, "No CEC devices found\n");
+    fflush(stderr);
+    libcecc_destroy(&g_iface);
+    return;
+  }
+  
+  strcpy(g_strPort, devices[0].comm);
+  if (!g_iface.open(g_iface.connection, g_strPort, 5000)) {
+    fprintf(stderr, "Unable to open the device on port %s\n", g_strPort);
+    fflush(stderr);
+    libcecc_destroy(&g_iface);
+    return;
+  }
+  
+  g_iface.set_active_source(g_iface.connection, g_config.deviceTypes.types[0]);
+}
+#endif
 
 static void input_init_parms(struct input_device *dev, struct input_abs_parms *parms, int code) {
   parms->flat = libevdev_get_abs_flat(dev->dev, code);
@@ -614,5 +705,8 @@ void input_map(char* fileName) {
 }
 
 void input_loop() {
+#ifdef HAVE_LIBCEC
+  init_cec();
+#endif
   input_poll(input_handle_event);
 }

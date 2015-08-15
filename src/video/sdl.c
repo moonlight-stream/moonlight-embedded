1dbb80954213248f26d7ddf0f6e88424bea91fa7
Iwan Timmer <irtimmer@gmail.com>
2015-08-15 19:51:59 +0200

    Enable frame invalidation for FFMpeg decoder

--- src/video/sdl.c
+++ src/video/sdl.c
@@ -94,5 +94,5 @@ DECODER_RENDERER_CALLBACKS decoder_callbacks_sdl = {
   .setup = sdl_setup,
   .cleanup = sdl_cleanup,
   .submitDecodeUnit = sdl_submit_decode_unit,
-  .capabilities = CAPABILITY_DIRECT_SUBMIT,
+  .capabilities = CAPABILITY_DIRECT_SUBMIT | CAPABILITY_REFERENCE_FRAME_INVALIDATION,
 };

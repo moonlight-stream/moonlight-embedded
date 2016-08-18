59173344edae1d1a1e71dd465ccb9ea0b2971f43
Ilya Zhuravlev <whatever@xyz.is>
2016-08-18 04:41:32 -0700

    vita: fix copyrights in files I've created

--- src/video/vita.c
+++ src/video/vita.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of Moonlight Embedded.
  *
- * Copyright (C) 2015 Iwan Timmer
+ * Copyright (C) 2016 Ilya Zhuravlev
  *
  * Moonlight is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

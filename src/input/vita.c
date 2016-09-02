3922223e63e101f3cf31b7eebc79825dde10db6f
Vasiliy Horbachenko <shadow.prince@ya.ru>
2016-09-02 18:23:41 +0300

    updated copyright

--- src/input/vita.c
+++ src/input/vita.c
@@ -1,7 +1,7 @@
 /*
  * This file is part of Moonlight Embedded.
  *
- * Copyright (C) 2016 Ilya Zhuravlev, Vasyl Horbachenko
+ * Copyright (C) 2016 Ilya Zhuravlev, Sunguk Lee, Vasyl Horbachenko
  *
  * Moonlight is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by

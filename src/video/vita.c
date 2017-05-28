0198eea1f95ca72905e053ad0d4a9c7c4dd3e395
Sunguk Lee <d3m3vilurr@gmail.com>
2017-05-29 00:40:41 +0900

    Remove duplicate function/struct defines
    vita-header support this.

--- src/video/vita.c
+++ src/video/vita.c
@@ -25,6 +25,7 @@
 #include <stdbool.h>
 #include <psp2/kernel/sysmem.h>
 #include <psp2/display.h>
+#include <psp2/videodec.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -44,112 +45,6 @@ enum {
 };
 int backbuffer;
 void *framebuffer[2];
-
-// https://gitlab.slkdev.net/RPCS3/rpcs3/blob/0e5c54709d7fa2a76b8944ca999b5b28a722be31/rpcs3/Emu/ARMv7/Modules/sceVideodec.h
-typedef struct SceVideodecQueryInitInfoHwAvcdec {
-  uint32_t size;
-  uint32_t horizontal;
-  uint32_t vertical;
-  uint32_t numOfRefFrames;
-  uint32_t numOfStreams;
-} SceVideodecQueryInitInfoHwAvcdec;
-
-typedef struct SceAvcdecQueryDecoderInfo {
-  uint32_t horizontal;
-  uint32_t vertical;
-  uint32_t numOfRefFrames;
-} SceAvcdecQueryDecoderInfo;
-
-typedef struct SceAvcdecDecoderInfo {
-  uint32_t frameMemSize;
-} SceAvcdecDecoderInfo;
-
-struct SceAvcdecBuf {
-  void *pBuf;
-  uint32_t size;
-};
-
-typedef struct SceAvcdecCtrl {
-  uint32_t handle;
-  struct SceAvcdecBuf frameBuf;
-} SceAvcdecCtrl;
-
-struct SceVideodecTimeStamp {
-  uint32_t upper;
-  uint32_t lower;
-};
-
-typedef struct SceAvcdecAu {
-  struct SceVideodecTimeStamp pts;
-  struct SceVideodecTimeStamp dts;
-  struct SceAvcdecBuf es;
-} SceAvcdecAu;
-
-struct SceAvcdecFrameOptionRGBA
-{
-  uint8_t alpha;
-  uint8_t cscCoefficient;
-  uint8_t reserved[14];
-};
-union SceAvcdecFrameOption
-{
-  uint8_t reserved[16];
-  struct SceAvcdecFrameOptionRGBA rgba;
-};
-struct SceAvcdecFrame {
-  uint32_t pixelType;
-  uint32_t framePitch;
-  uint32_t frameWidth;
-  uint32_t frameHeight;
-
-  uint32_t horizontalSize;
-  uint32_t verticalSize;
-
-  uint32_t frameCropLeftOffset;
-  uint32_t frameCropRightOffset;
-  uint32_t frameCropTopOffset;
-  uint32_t frameCropBottomOffset;
-
-  union SceAvcdecFrameOption opt;
-
-  void *pPicture[2];
-};
-struct SceAvcdecInfo
-{
-  uint32_t numUnitsInTick;
-  uint32_t timeScale;
-  uint8_t fixedFrameRateFlag;
-
-  uint8_t aspectRatioIdc;
-  uint16_t sarWidth;
-  uint16_t sarHeight;
-
-  uint8_t colourPrimaries;
-  uint8_t transferCharacteristics;
-  uint8_t matrixCoefficients;
-uint8_t videoFullRangeFlag;
-
-  uint8_t padding[3];
-
-  struct SceVideodecTimeStamp pts;
-};
-struct SceAvcdecPicture {
-  uint32_t size;
-  struct SceAvcdecFrame frame;
-  struct SceAvcdecInfo info;
-};
-
-typedef struct SceAvcdecArrayPicture {
-  uint32_t numOfOutput;
-  uint32_t numOfElm;
-  struct SceAvcdecPicture **pPicture;
-} SceAvcdecArrayPicture;
-
-int sceVideodecInitLibrary(uint32_t codec, const SceVideodecQueryInitInfoHwAvcdec *initInfo);
-int sceAvcdecQueryDecoderMemSize(uint32_t codec, const SceAvcdecQueryDecoderInfo *query, SceAvcdecDecoderInfo *decoderInfo);
-int sceAvcdecCreateDecoder(uint32_t codec, SceAvcdecCtrl *decoder, const SceAvcdecQueryDecoderInfo *query);
-int sceAvcdecDecode(SceAvcdecCtrl *decoder, SceAvcdecAu *au, SceAvcdecArrayPicture *array_picture);
-
 
 SceAvcdecCtrl decoder = {0};
 

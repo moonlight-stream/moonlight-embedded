697bee2954d9f67df2bc62870ec6c7fd68de4677
Iwan Timmer <irtimmer@gmail.com>
2017-06-04 14:48:26 +0200

    Read all pending X11 events during input handling

--- src/input/x11.c
+++ src/input/x11.c
@@ -46,86 +46,85 @@ static Cursor cursor;
 static bool grabbed = True;
 
 static int x11_handler(int fd) {
-  if (!XPending(display))
-    return LOOP_OK;
-
   XEvent event;
   int button = 0;
   int motion_x, motion_y;
 
-  XNextEvent(display, &event);
-  switch (event.type) {
-  case KeyPress:
-  case KeyRelease:
-    if (event.xkey.keycode >= 8 && event.xkey.keycode < (sizeof(keyCodes)/sizeof(keyCodes[0]) + 8)) {
-      if ((keyboard_modifiers & MODIFIERS) == MODIFIERS && event.type == KeyRelease) {
-        grabbed = !grabbed;
-        XDefineCursor(display, window, grabbed ? cursor : 0);
+  while (XPending(display)) {
+    XNextEvent(display, &event);
+    switch (event.type) {
+    case KeyPress:
+    case KeyRelease:
+      if (event.xkey.keycode >= 8 && event.xkey.keycode < (sizeof(keyCodes)/sizeof(keyCodes[0]) + 8)) {
+        if ((keyboard_modifiers & MODIFIERS) == MODIFIERS && event.type == KeyRelease) {
+          grabbed = !grabbed;
+          XDefineCursor(display, window, grabbed ? cursor : 0);
+        }
+
+        int modifier = 0;
+        switch (event.xkey.keycode) {
+        case 0x32:
+        case 0x3e:
+          modifier = MODIFIER_SHIFT;
+          break;
+        case 0x40:
+        case 0x6c:
+          modifier = MODIFIER_ALT;
+          break;
+        case 0x25:
+        case 0x69:
+          modifier = MODIFIER_CTRL;
+          break;
+        }
+
+        if (modifier != 0) {
+          if (event.type == KeyPress)
+            keyboard_modifiers |= modifier;
+          else
+            keyboard_modifiers &= ~modifier;
+        }
+
+        short code = 0x80 << 8 | keyCodes[event.xkey.keycode - 8];
+        LiSendKeyboardEvent(code, event.type == KeyPress ? KEY_ACTION_DOWN : KEY_ACTION_UP, keyboard_modifiers);
+      }
+      break;
+    case ButtonPress:
+    case ButtonRelease:
+      switch (event.xbutton.button) {
+      case Button1:
+        button = BUTTON_LEFT;
+        break;
+      case Button2:
+        button = BUTTON_MIDDLE;
+        break;
+      case Button3:
+        button = BUTTON_RIGHT;
+        break;
       }
 
-      int modifier = 0;
-      switch (event.xkey.keycode) {
-      case 0x32:
-      case 0x3e:
-        modifier = MODIFIER_SHIFT;
-        break;
-      case 0x40:
-      case 0x6c:
-        modifier = MODIFIER_ALT;
-        break;
-      case 0x25:
-      case 0x69:
-        modifier = MODIFIER_CTRL;
-        break;
+      if (button != 0)
+        LiSendMouseButtonEvent(event.type==ButtonPress ? BUTTON_ACTION_PRESS : BUTTON_ACTION_RELEASE, button);
+      break;
+    case MotionNotify:
+      motion_x = event.xmotion.x - last_x;
+      motion_y = event.xmotion.y - last_y;
+      if (abs(motion_x) > 0 || abs(motion_y) > 0) {
+        if (last_x >= 0 && last_y >= 0)
+          LiSendMouseMoveEvent(motion_x, motion_y);
+
+        if (grabbed)
+          XWarpPointer(display, None, window, 0, 0, 0, 0, 640, 360);
       }
 
-      if (modifier != 0) {
-        if (event.type == KeyPress)
-          keyboard_modifiers |= modifier;
-        else
-          keyboard_modifiers &= ~modifier;
-      }
+      last_x = grabbed ? 640 : event.xmotion.x;
+      last_y = grabbed ? 360 : event.xmotion.y;
+      break;
+    case ClientMessage:
+      if (event.xclient.data.l[0] == wm_deletemessage)
+        return LOOP_RETURN;
 
-      short code = 0x80 << 8 | keyCodes[event.xkey.keycode - 8];
-      LiSendKeyboardEvent(code, event.type == KeyPress ? KEY_ACTION_DOWN : KEY_ACTION_UP, keyboard_modifiers);
-    }
-    break;
-  case ButtonPress:
-  case ButtonRelease:
-    switch (event.xbutton.button) {
-    case Button1:
-      button = BUTTON_LEFT;
-      break;
-    case Button2:
-      button = BUTTON_MIDDLE;
-      break;
-    case Button3:
-      button = BUTTON_RIGHT;
       break;
     }
-
-    if (button != 0)
-      LiSendMouseButtonEvent(event.type==ButtonPress ? BUTTON_ACTION_PRESS : BUTTON_ACTION_RELEASE, button);
-    break;
-  case MotionNotify:
-    motion_x = event.xmotion.x - last_x;
-    motion_y = event.xmotion.y - last_y;
-    if (abs(motion_x) > 0 || abs(motion_y) > 0) {
-      if (last_x >= 0 && last_y >= 0)
-        LiSendMouseMoveEvent(motion_x, motion_y);
-
-      if (grabbed)
-        XWarpPointer(display, None, window, 0, 0, 0, 0, 640, 360);
-    }
-
-    last_x = grabbed ? 640 : event.xmotion.x;
-    last_y = grabbed ? 360 : event.xmotion.y;
-    break;
-  case ClientMessage:
-    if (event.xclient.data.l[0] == wm_deletemessage)
-      return LOOP_RETURN;
-
-    break;
   }
 
   return LOOP_OK;

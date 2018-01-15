moonlight-stream/moonlight-embedded

commit 4a0c3ba5e2d3d029e86e92d0ac4de8dc3bc3c27a

Check status code for GS api requests

src/gui/ui_connect.c
@@ -238,6 +238,9 @@ int ui_connect(char *address) {
         display_error("Unsupported version: %s\n", gs_error);
         return 0;
       }
+    } else if (ret == GS_ERROR) {
+      display_error("Gamestream error: %s\n", gs_error);
+      return 0;
     } else if (ret != GS_OK) {
       display_error("Can't connect to server\n%s", address);
       return 0;

libgamestream/client.c
@@ -211,6 +211,11 @@ static int load_server_status(PSERVER_DATA server) {
       goto cleanup;
     }
 
+    if (xml_status(data->memory, data->size) == GS_ERROR) {
+      ret = GS_ERROR;
+      goto cleanup;
+    }
+
     if (xml_search(data->memory, data->size, "currentgame", &currentGameText) != GS_OK) {
       goto cleanup;
     }
@@ -420,7 +425,9 @@ int gs_pair(PSERVER_DATA server, char* pin) {
   else if ((ret = http_request(url, data)) != GS_OK)
     goto cleanup;
 
-  if ((ret = xml_search(data->memory, data->size, "paired", &result)) != GS_OK)
+  if ((ret = xml_status(data->memory, data->size) != GS_OK))
+    goto cleanup;
+  else if ((ret = xml_search(data->memory, data->size, "paired", &result)) != GS_OK)
     goto cleanup;
 
   if (strcmp(result, "1") != 0) {
@@ -477,7 +484,9 @@ int gs_pair(PSERVER_DATA server, char* pin) {
 
   free(result);
   result = NULL;
-  if ((ret = xml_search(data->memory, data->size, "paired", &result)) != GS_OK)
+  if ((ret = xml_status(data->memory, data->size) != GS_OK))
+    goto cleanup;
+  else if ((ret = xml_search(data->memory, data->size, "paired", &result)) != GS_OK)
     goto cleanup;
 
   if (strcmp(result, "1") != 0) {
@@ -531,7 +540,9 @@ int gs_pair(PSERVER_DATA server, char* pin) {
 
   free(result);
   result = NULL;
-  if ((ret = xml_search(data->memory, data->size, "paired", &result)) != GS_OK)
+  if ((ret = xml_status(data->memory, data->size) != GS_OK))
+    goto cleanup;
+  else if ((ret = xml_search(data->memory, data->size, "paired", &result)) != GS_OK)
     goto cleanup;
 
   if (strcmp(result, "1") != 0) {
@@ -580,7 +591,9 @@ int gs_pair(PSERVER_DATA server, char* pin) {
 
   free(result);
   result = NULL;
-  if ((ret = xml_search(data->memory, data->size, "paired", &result)) != GS_OK)
+  if ((ret = xml_status(data->memory, data->size) != GS_OK))
+    goto cleanup;
+  else if ((ret = xml_search(data->memory, data->size, "paired", &result)) != GS_OK)
     goto cleanup;
 
   if (strcmp(result, "1") != 0) {
@@ -597,7 +610,9 @@ int gs_pair(PSERVER_DATA server, char* pin) {
 
   free(result);
   result = NULL;
-  if ((ret = xml_search(data->memory, data->size, "paired", &result)) != GS_OK)
+  if ((ret = xml_status(data->memory, data->size) != GS_OK))
+    goto cleanup;
+  else if ((ret = xml_search(data->memory, data->size, "paired", &result)) != GS_OK)
     goto cleanup;
 
   if (strcmp(result, "1") != 0) {
@@ -634,6 +649,8 @@ int gs_applist(PSERVER_DATA server, PAPP_LIST *list) {
   sprintf(url, "https://%s:47984/applist?uniqueid=%s&uuid=%s", server->serverInfo.address, unique_id, uuid_str);
   if (http_request(url, data) != GS_OK)
     ret = GS_IO_ERROR;
+  else if (xml_status(data->memory, data->size) == GS_ERROR)
+    ret = GS_ERROR;
   else if (xml_applist(data->memory, data->size, list) != GS_OK)
     ret = GS_INVALID;
 
@@ -692,7 +709,9 @@ int gs_start_app(PSERVER_DATA server, STREAM_CONFIGURATION *config, int appId, b
     goto cleanup;
   printf("ret = 0x%x\n", ret);
 
-  if ((ret = xml_search(data->memory, data->size, "gamesession", &result)) != GS_OK)
+  if ((ret = xml_status(data->memory, data->size) != GS_OK))
+    goto cleanup;
+  else if ((ret = xml_search(data->memory, data->size, "gamesession", &result)) != GS_OK)
     goto cleanup;
 
   if (!strcmp(result, "0")) {
@@ -724,7 +743,9 @@ int gs_quit_app(PSERVER_DATA server) {
   if ((ret = http_request(url, data)) != GS_OK)
     goto cleanup;
 
-  if ((ret = xml_search(data->memory, data->size, "cancel", &result)) != GS_OK)
+  if ((ret = xml_status(data->memory, data->size) != GS_OK))
+    goto cleanup;
+  else if ((ret = xml_search(data->memory, data->size, "cancel", &result)) != GS_OK)
     goto cleanup;
 
   if (strcmp(result, "0") == 0) {

libgamestream/errors.h
@@ -28,5 +28,6 @@
 #define GS_NOT_SUPPORTED_4K -6
 #define GS_UNSUPPORTED_VERSION -7
 #define GS_NOT_SUPPORTED_MODE -8
+#define GS_ERROR -9
 
-const char* gs_error;
+extern const char* gs_error;

libgamestream/xml.c
@@ -23,6 +23,8 @@
 #include <expat.h>
 #include <string.h>
 
+#define STATUS_OK 200
+
 static XML_Parser parser;
 
 struct xml_query {
@@ -110,6 +112,23 @@ static void XMLCALL _xml_end_mode_element(void *userData, const char *name) {
   }
 }
 
+static void XMLCALL _xml_start_status_element(void *userData, const char *name, const char **atts) {
+  if (strcmp("root", name) == 0) {
+    int* status = (int*) userData;
+    for (int i = 0; atts[i]; i += 2) {
+      if (strcmp("status_code", atts[i]) == 0)
+        *status = atoi(atts[i + 1]);
+      else if (*status != STATUS_OK && strcmp("status_message", atts[i]) == 0) {
+        gs_error = malloc(strlen(atts[i + 1]));
+        if (gs_error)
+          strcpy((char*) gs_error, atts[i + 1]);
+      }
+    }
+  }
+}
+
+static void XMLCALL _xml_end_status_element(void *userData, const char *name) { }
+
 static void XMLCALL _xml_write_data(void *userData, const XML_Char *s, int len) {
   struct xml_query *search = (struct xml_query*) userData;
   if (search->start > 0) {
@@ -192,4 +211,20 @@ int xml_modelist(char* data, size_t len, PDISPLAY_MODE *mode_list) {
 
   return GS_OK;
 
+}
+
+int xml_status(char* data, size_t len) {
+  int status = 0;
+  XML_Parser parser = XML_ParserCreate("UTF-8");
+  XML_SetUserData(parser, &status);
+  XML_SetElementHandler(parser, _xml_start_status_element, _xml_end_status_element);
+  if (!XML_Parse(parser, data, len, 1)) {
+    int code = XML_GetErrorCode(parser);
+    gs_error = XML_ErrorString(code);
+    XML_ParserFree(parser);
+    return GS_INVALID;
+  }
+
+  XML_ParserFree(parser);
+  return status == STATUS_OK ? GS_OK : GS_ERROR;
 }

libgamestream/xml.h
@@ -36,3 +36,4 @@ typedef struct _DISPLAY_MODE {
 int xml_search(char* data, size_t len, char* node, char** result);
 int xml_applist(char* data, size_t len, PAPP_LIST *app_list);
 int xml_modelist(char* data, size_t len, PDISPLAY_MODE *mode_list);
+int xml_status(char* data, size_t len);

commit b838278ba0b2c4a8af02eb4c7cd48f17165fe950

gui: Fix cannot connect GFE with new device
an issue introduced from 4a0c3ba5e2d
in before, gs_applist always succeed whenever with pair or unpair.
but now, gs_applist required paired device.

Resolve #106

src/gui/ui_connect.c
@@ -224,9 +224,10 @@ disconnect:
 }
 
 int ui_connect(char *address) {
+  int ret;
   if (!connection_is_ready()) {
     flash_message("Connecting to:\n %s...", address);
-    int ret = gs_init(&server, address, config.key_dir);
+    ret = gs_init(&server, address, config.key_dir);
     if (ret == GS_OUT_OF_MEMORY) {
       display_error("Not enough memory");
       return 0;
@@ -246,21 +247,23 @@ int ui_connect(char *address) {
       return 0;
     }
 
+    connection_reset();
+  }
+
+  int app_count = 0;
+  if (server.paired) {
     ret = gs_applist(&server, &server_applist);
     if (ret != GS_OK) {
       display_error("Can't get applist!\n%d\n%s", ret, gs_error);
       return 0;
     }
 
-    connection_reset();
-  }
-
-  int app_count = 0;
-  if (server_applist != NULL) {
-    PAPP_LIST list = server_applist;
-    while (list) {
-      list = list->next;
-      app_count += 1;
+    if (server_applist != NULL) {
+      PAPP_LIST list = server_applist;
+      while (list) {
+        list = list->next;
+        app_count += 1;
+      }
     }
   }
 
@@ -298,47 +301,52 @@ int ui_connect(char *address) {
   MENU_MESSAGE(server_info, 0xffffffff);
   MENU_MESSAGE("", 0);
 
-  // current stream
-  if (server.currentGame != 0) {
-    char current_appname[256];
-    char current_status[256];
-
-    if (!get_app_name(server_applist, server.currentGame, current_appname)) {
-      strcpy(current_appname, "unknown");
-    }
-    sprintf(current_status, "Streaming %s", current_appname);
-
-    MENU_CATEGORY(current_status);
-    MENU_ENTRY(server.currentGame, "Resume");
-    MENU_ENTRY(CONNECT_QUITAPP, "Quit");
-  }
-
-  // pairing
-  MENU_CATEGORY(server.paired ? "Paired" : "Not paired");
-  if (server.paired) {
-    connection_paired();
-    MENU_ENTRY(CONNECT_PAIRUNPAIR, "Unpair");
-  } else {
+  if (!server.paired) {
+    // pairing
+    MENU_CATEGORY("Not paired");
     MENU_ENTRY(CONNECT_PAIRUNPAIR, "Pair");
-  }
 
-  MENU_ENTRY(CONNECT_DISCONNECT, "Disconnect");
+    MENU_ENTRY(CONNECT_DISCONNECT, "Disconnect");
+  } else {
+    // current stream
+    if (server.currentGame != 0) {
+      char current_appname[256];
+      char current_status[256];
 
-  // app list
-  if (server_applist != NULL) {
-    MENU_CATEGORY("Applications");
+      if (!get_app_name(server_applist, server.currentGame, current_appname)) {
+        strcpy(current_appname, "unknown");
+      }
+      sprintf(current_status, "Streaming %s", current_appname);
 
-    pos[0] = idx;
-
-    PAPP_LIST list = server_applist;
-    while (list) {
-      MENU_ENTRY(list->id, list->name);
-      list = list->next;
+      MENU_CATEGORY(current_status);
+      MENU_ENTRY(server.currentGame, "Resume");
+      MENU_ENTRY(CONNECT_QUITAPP, "Quit");
     }
 
-    pos[1] = idx;
-  } else {
-    pos[0] = -1;
+    // pairing
+    MENU_CATEGORY("Paired");
+    connection_paired();
+    // FIXME: unpair not work
+    // MENU_ENTRY(CONNECT_PAIRUNPAIR, "Unpair");
+
+    MENU_ENTRY(CONNECT_DISCONNECT, "Disconnect");
+
+    // app list
+    if (server_applist != NULL) {
+      MENU_CATEGORY("Applications");
+
+      pos[0] = idx;
+
+      PAPP_LIST list = server_applist;
+      while (list) {
+        MENU_ENTRY(list->id, list->name);
+        list = list->next;
+      }
+
+      pos[1] = idx;
+    } else {
+      pos[0] = -1;
+    }
   }
 
   return display_menu(menu, idx, NULL, &ui_connect_loop, NULL, NULL, &menu);
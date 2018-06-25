f95494013b7bda81a387ef7bb77a478da3247777
Sajid Anwar <sajidanwar94@gmail.com>
2018-06-24 18:54:17 -0700

    Update dependencies for Windows usage

--- dependencies/dependencies.sh
+++ dependencies/dependencies.sh
@@ -1,16 +1,29 @@
 #!/bin/sh
 
+if [[ -n $(command -v pacman) ]]; then
+    PACMAN="pacman"
+    MAKEPKG="makepkg"
+    TOUCH="touch"
+elif [[ -n $(command -v dkp-pacman) ]]; then 
+    PACMAN="sudo dkp-pacman"
+    MAKEPKG="dkp-makepkg"
+    TOUCH="sudo touch"
+else 
+    echo "ERROR: Could not find pacman (or dkp-pacman); is $DEVKITPRO set and on the path?"
+    exit 1
+fi
+
 echo "Adding missing <sys/termios.h> to devkitA64"
-sudo touch $DEVKITPRO/devkitA64/aarch64-none-elf/include/sys/termios.h
+$(TOUCH) $DEVKITPRO/devkitA64/aarch64-none-elf/include/sys/termios.h
 
 echo "Installing necessary dependencies for moonlight-switch"
-sudo dkp-pacman -S --needed --noconfirm \
+$PACMAN -S --needed --noconfirm \
     switch-pkg-config devkitpro-pkgbuild-helpers \
     switch-sdl2 switch-sdl2_gfx switch-sdl2_ttf switch-sdl2_image \
     switch-bzip2 switch-zlib
 
 echo "Building and installing the bundled dependencies"
-(cd switch-libexpat; dkp-makepkg --clean --needed --noconfirm; sudo dkp-pacman -U --needed --noconfirm switch-libexpat*)
-(cd switch-libavcodec; dkp-makepkg --clean --needed --noconfirm; sudo dkp-pacman -U --needed --noconfirm switch-libavcodec*)
-(cd switch-libopus; dkp-makepkg --clean --needed --noconfirm; sudo dkp-pacman -U --needed --noconfirm switch-libopus*)
-(cd switch-openssl; dkp-makepkg --clean --needed --noconfirm; sudo dkp-pacman -U --needed --noconfirm switch-openssl*)
+(cd switch-libexpat; $MAKEPKG --clean --needed --noconfirm; $PACMAN -U --needed --noconfirm switch-libexpat*)
+(cd switch-libavcodec; $MAKEPKG --clean --needed --noconfirm; $PACMAN -U --needed --noconfirm switch-libavcodec*)
+(cd switch-libopus; $MAKEPKG --clean --needed --noconfirm; $PACMAN -U --needed --noconfirm switch-libopus*)
+(cd switch-openssl; $MAKEPKG --clean --needed --noconfirm; $PACMAN -U --needed --noconfirm switch-openssl*)

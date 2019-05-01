de4b87efacf18f681fa60c74ec73a6d5d9c5b019
Cameron Gutman <aicommander@gmail.com>
2019-04-30 19:50:22 -0700

    Fix bugs found by compiler warnings

--- src/loop.c
+++ src/loop.c
@@ -81,14 +81,15 @@ void loop_remove_fd(int fd) {
   int fdindex;
   
   for (int i=0;i<numFds;i++) {
-    if (fds[i].fd = fd)
+    if (fds[i].fd == fd) {
       fdindex = i;
       break;
+    }
   }
   
   if (fdindex != numFds && numFds > 0) {
     memcpy(&fds[fdindex], &fds[numFds], sizeof(struct pollfd));
-    memcpy(&fdHandlers[fdindex], &fdHandlers[numFds], sizeof(FdHandler*));
+    memcpy(&fdHandlers[fdindex], &fdHandlers[numFds], sizeof(FdHandler));
   }
   #endif
 }

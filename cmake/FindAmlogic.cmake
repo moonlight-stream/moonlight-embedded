3859949b0f5939511d8339b5f904da52216b0004
Cameron Gutman <aicommander@gmail.com>
2022-09-23 21:44:22 -0500

    Fix amcodec detection on OSMC

--- cmake/FindAmlogic.cmake
+++ cmake/FindAmlogic.cmake
@@ -1,25 +1,25 @@
 find_path(AMLOGIC_INCLUDE_DIR
   NAMES codec.h
   DOC "Amlogic include directory"
-  PATHS /usr/local/include/amcodec /usr/include/amcodec /usr/include/)
+  PATHS /usr/local/include/amcodec /usr/osmc/include/amcodec /usr/include/amcodec /usr/include/)
 mark_as_advanced(AMLOGIC_INCLUDE_DIR)
 
 find_library(AMAVUTILS_LIBRARY
   NAMES libamavutils.so
   DOC "Path to Amlogic Audio Video Utils Library"
-  PATHS /usr/lib/aml_libs /usr/local/lib /usr/lib)
+  PATHS /usr/lib/aml_libs /usr/osmc/lib /usr/local/lib /usr/lib)
 mark_as_advanced(AMAVUTILS_LIBRARY)
 
 find_library(AMADEC_LIBRARY
   NAMES libamadec.so
   DOC "Path to Amlogic Audio Decoder Library"
-  PATHS /usr/lib/aml_libs /usr/local/lib /usr/lib)
+  PATHS /usr/lib/aml_libs /usr/osmc/lib /usr/local/lib /usr/lib)
 mark_as_advanced(AMADEC_LIBRARY)
 
 find_library(AMCODEC_LIBRARY
   NAMES libamcodec.so
   DOC "Path to Amlogic Video Codec Library"
-  PATHS /usr/lib/aml_libs /usr/local/lib /usr/lib)
+  PATHS /usr/lib/aml_libs /usr/osmc/lib /usr/local/lib /usr/lib)
 mark_as_advanced(AMCODEC_LIBRARY)
 
 include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)

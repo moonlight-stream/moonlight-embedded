4d2ac89805985b6926ee5d1f553b12172ae1f348
TheChoconut <thechoconut@outlook.com>
2022-11-18 00:44:57 +0100

    lazy load libamcodec, only require libamcodec.so

--- cmake/FindAmlogic.cmake
+++ cmake/FindAmlogic.cmake
@@ -4,18 +4,6 @@ find_path(AMLOGIC_INCLUDE_DIR
   PATHS /usr/local/include/amcodec /usr/osmc/include/amcodec /usr/include/amcodec /usr/include/)
 mark_as_advanced(AMLOGIC_INCLUDE_DIR)
 
-find_library(AMAVUTILS_LIBRARY
-  NAMES libamavutils.so
-  DOC "Path to Amlogic Audio Video Utils Library"
-  PATHS /usr/lib/aml_libs /usr/osmc/lib /usr/local/lib /usr/lib)
-mark_as_advanced(AMAVUTILS_LIBRARY)
-
-find_library(AMADEC_LIBRARY
-  NAMES libamadec.so
-  DOC "Path to Amlogic Audio Decoder Library"
-  PATHS /usr/lib/aml_libs /usr/osmc/lib /usr/local/lib /usr/lib)
-mark_as_advanced(AMADEC_LIBRARY)
-
 find_library(AMCODEC_LIBRARY
   NAMES libamcodec.so
   DOC "Path to Amlogic Video Codec Library"
@@ -23,7 +11,7 @@ find_library(AMCODEC_LIBRARY
 mark_as_advanced(AMCODEC_LIBRARY)
 
 include(${CMAKE_ROOT}/Modules/FindPackageHandleStandardArgs.cmake)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(Amlogic DEFAULT_MSG AMLOGIC_INCLUDE_DIR AMCODEC_LIBRARY AMADEC_LIBRARY AMAVUTILS_LIBRARY)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Amlogic DEFAULT_MSG AMLOGIC_INCLUDE_DIR AMCODEC_LIBRARY)
 
-set(AMLOGIC_LIBRARIES ${AMCODEC_LIBRARY} ${AMADEC_LIBRARY} ${AMAVUTILS_LIBRARY})
+set(AMLOGIC_LIBRARIES ${AMCODEC_LIBRARY})
 set(AMLOGIC_INCLUDE_DIRS ${AMLOGIC_INCLUDE_DIR})

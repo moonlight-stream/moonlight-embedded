147bba0f85fa5678e45513e6296ec47ab85cbf75
armin-25689 <83564821+armin-25689@users.noreply.github.com>
2023-12-31 15:22:43 +0800

    fix: it took so long time to connect to the Sunshine host in the FreeBSD
    Disable reuse feature for FreeBSD to prevent excessive time from being spent connecting to the Sunshine host

--- libgamestream/http.c
+++ libgamestream/http.c
@@ -73,6 +73,9 @@ int http_init(const char* keyDirectory, int logLevel) {
 int http_request(char* url, PHTTP_DATA data) {
   curl_easy_setopt(curl, CURLOPT_WRITEDATA, data);
   curl_easy_setopt(curl, CURLOPT_URL, url);
+#ifdef __FreeBSD__
+  curl_easy_setopt(curl, CURLOPT_FORBID_REUSE, 1);
+#endif
 
   if (debug)
     printf("Request %s\n", url);

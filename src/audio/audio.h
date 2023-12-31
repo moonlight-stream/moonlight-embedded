3840b1409ee120d6c1c94914791a6a9fa3bb4e9f
armin-25689 <83564821+armin-25689@users.noreply.github.com>
2023-12-31 15:14:22 +0800

    feat: add oss audio callback for FreeBSD
    OSS is the native sound system of the FreeBSD.It will be used on the x11_vaapi platform.It was added to reduce moonlight's dependency on FreeBSD.

--- src/audio/audio.h
+++ src/audio/audio.h
@@ -31,3 +31,6 @@ extern AUDIO_RENDERER_CALLBACKS audio_callbacks_sdl;
 extern AUDIO_RENDERER_CALLBACKS audio_callbacks_pulse;
 bool audio_pulse_init(char* audio_device);
 #endif
+#ifdef __FreeBSD__
+extern AUDIO_RENDERER_CALLBACKS audio_callbacks_oss;
+#endif
